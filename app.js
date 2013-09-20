d7ebf4465f327c5c182da6fdde01416ec3b959d8
Edward Look <edwlook@gmail.com>
2013-09-19 19:53:39 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+//express + socket.io Server
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+io.sockets.on('connection', function (socket) {
+    //socket.emit('message', { message: 'Welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
+server.listen(app.get('port'), function() {
+	console.log('Server started on port', app.get('port'));
+});
+
